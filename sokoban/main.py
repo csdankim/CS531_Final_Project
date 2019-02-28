c20009c3ad09842897bac4ea62eb6fdac93970c1
Evan Steele <evan@evansteele.net>
2019-02-27 17:03:47 -0800

    first draft of MD heuristic

--- sokoban/main.py
+++ sokoban/main.py
@@ -1,5 +1,6 @@
 import board
 import actions
+import heuristic
 
 new_board = board.gen_default_board()
 board.draw_board(new_board)
@@ -9,4 +10,4 @@ print("\n")
 print("Frontier:")
 frontier = board.gen_frontier(new_board)
 board.draw_frontier(frontier)
-
+heuristic.manhattan_distance(frontier)
