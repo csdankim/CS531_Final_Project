e253f12cb0e4cd81e4af3234d314b66d99f8d784
steelee <esteele101@gmail.com>
2019-03-01 11:29:58 -0800

    Forgot to remove some testing elements

--- sokoban/main.py
+++ sokoban/main.py
@@ -7,15 +7,6 @@ new_board = board.gen_default_board()
 board.draw_board(new_board)
 print("\n")
 
-# generate the frontier nodes
-frontier = board.gen_frontier(new_board)
-frontier = board.gen_frontier(frontier[3])
-frontier = board.gen_frontier(frontier[3])
-frontier = board.gen_frontier(frontier[1])
-frontier = board.gen_frontier(frontier[1])
-frontier = board.gen_frontier(frontier[5])
-frontier = board.gen_frontier(frontier[1])
-#board.draw_frontier(frontier)
-#print(board.check_goal(frontier[3]))
-
+# After some manual testing I found that it takes
+# 7 moves at best to reach it
 result = ida.ida_run(new_board, 7)
