fdb73bc70481a10bccdc757cac7946253f953b8d
steelee <esteele101@gmail.com>
2019-03-01 11:27:46 -0800

    IDA* function implemented and tested

--- sokoban/main.py
+++ sokoban/main.py
@@ -1,13 +1,21 @@
 import board
 import actions
 import heuristic
+import ida
 
 new_board = board.gen_default_board()
 board.draw_board(new_board)
 print("\n")
 
 # generate the frontier nodes
-print("Frontier:")
 frontier = board.gen_frontier(new_board)
-board.draw_frontier(frontier)
-heuristic.manhattan_distance(frontier)
+frontier = board.gen_frontier(frontier[3])
+frontier = board.gen_frontier(frontier[3])
+frontier = board.gen_frontier(frontier[1])
+frontier = board.gen_frontier(frontier[1])
+frontier = board.gen_frontier(frontier[5])
+frontier = board.gen_frontier(frontier[1])
+#board.draw_frontier(frontier)
+#print(board.check_goal(frontier[3]))
+
+result = ida.ida_run(new_board, 7)
