e59fe60c4589d73c2072844741e8903ef6faf08d
steelee <esteele101@gmail.com>
2019-03-01 11:50:47 -0800

    Changed print statements a bit

--- sokoban/main.py
+++ sokoban/main.py
@@ -9,4 +9,5 @@ print("\n")
 
 # After some manual testing I found that it takes
 # 7 moves at best to reach it
+print("Running IDA* with Manhattan Distance heuristic")
 result = ida.ida_run(new_board, 7)
