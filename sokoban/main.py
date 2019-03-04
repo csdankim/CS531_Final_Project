a9b031f172c5c496d7847771623edeb2a553e5e4
steelee <evan@evansteele.net>
2019-03-04 14:28:27 -0800

    fixed incorrect depth assignment in rbfs

--- sokoban/main.py
+++ sokoban/main.py
@@ -11,6 +11,6 @@ print("\n")
 # After some manual testing I found that it takes
 # 7 moves at best to reach it
 print("Running IDA* with Manhattan Distance heuristic")
-result = ida.ida_run(new_board, 7)
+ida.ida_run(new_board, 7)
 print("Running RBFS with Manhattan Distance heurisitc")
-result = rbfs.rbfs_run(new_board)
+rbfs.rbfs_run(new_board)
--- sokoban/rbfs.py
+++ sokoban/rbfs.py
@@ -7,9 +7,10 @@ infinity = math.inf
 
 
 class F_node:
-    def __init__(self,f_value,node):
+    def __init__(self,f_value,node,cost):
         self.f = f_value
         self.n = node
+        self.c = cost
 
     def __lt__(self, other):
         return self.f < other.f
@@ -20,27 +21,30 @@ class F_node:
 
 def rbfs_run(evaluation_board):
     # reflects the layout of the RBFS evaluations
-    initial_board = F_node(0, evaluation_board)
-    return rbfs_search_function(initial_board, infinity)
+    initial_board = F_node(0, evaluation_board, 0)
+    rbfs_search_function(initial_board, infinity, 0)
 
 
-def rbfs_search_function(f_node, f_limit):
+def rbfs_search_function(f_node, f_limit, depth):
     if board.check_goal(f_node.n):
         print("Goal Achieved:")
-        return f_node.n
+        board.draw_board(f_node.n)
+        return "SUCCESS", f_node.f
     frontier = board.gen_frontier(f_node.n)
     if len(frontier) == 0:
         return "FAILURE", infinity
     successors = []
     for i in range(1, len(frontier), 2):
-        print(frontier[i])
-        successors.append(F_node(max((1 + heuristic.manhattan_distance_node(frontier[i])), f_node.f), frontier[i]))
+        successors.append(F_node(max((depth + heuristic.manhattan_distance_node(frontier[i])), f_node.f), frontier[i], depth))
     while True:
         successors.sort()
         best_node = successors[0]
         if best_node.f > f_limit:
             return "FAILURE", best_node.f
-        alternative = successors[1]
-        result, best_node.f = rbfs_search_function(best_node, min(f_limit, alternative.f))
+        if len(successors) == 1:
+            alternative = successors[0]
+        else:
+            alternative = successors[1]
+        result, best_node.f = rbfs_search_function(best_node, min(f_limit, alternative.f),depth+1)
         if result != "FAILURE":
-            return result
+            return result, best_node.f
