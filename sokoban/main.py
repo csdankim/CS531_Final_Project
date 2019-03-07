b0071198ebe3db6d7dddeda3adc80fb121589476
steelee <esteele101@gmail.com>
2019-03-07 10:58:38 -0800

    Added board, command line arguments, and modified IDA*

--- sokoban/main.py
+++ sokoban/main.py
@@ -4,22 +4,28 @@ import heuristic
 import ida
 import rbfs
 
-# specify board file here
-board_selection = "easy_board.txt"
-ida_limit = 40
+import argparse
 
-new_board = board.gen_default_board(board_selection)
+
+parser = argparse.ArgumentParser(description='Solve Sobokan puzzles')
+parser.add_argument("-m", "--map",
+                    dest="map_choice",
+                    default="trivial_board.txt",
+                    help="Choose a map file (default: trivial_board.txt)")
+
+args = parser.parse_args()
+new_board = board.gen_default_board(args.map_choice)
 board.draw_board(new_board)
 print("\n")
 
 print("Running IDA* with Manhattan Distance heuristic")
-ida.ida_run(new_board, ida_limit,"MD")
+ida.ida_run(new_board,"MD")
 print("Running IDA* with Hungarian heuristic")
-new_board = board.gen_default_board(board_selection)
-ida.ida_run(new_board, ida_limit,"HU")
+new_board = board.gen_default_board(args.map_choice)
+ida.ida_run(new_board,"HU")
 print("Running RBFS with Manhattan Distance heurisitc")
-new_board = board.gen_default_board(board_selection)
+new_board = board.gen_default_board(args.map_choice)
 rbfs.rbfs_run(new_board,"MD")
 print("Running RBFS with Hungarian heurisitc")
-new_board = board.gen_default_board(board_selection)
+new_board = board.gen_default_board(args.map_choice)
 rbfs.rbfs_run(new_board,"HU")
