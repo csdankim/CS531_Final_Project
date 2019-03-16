9fa62f8af4b03941ea4b1a03f428546e9f8c6291
Your Name <you@your-domain.com>
2019-03-15 21:53:45 -0700

    Final version for the submission
    UCT expansion fixed.
    deadlock detector applied to MCTS
    
    Everything works fine.

--- sokoban/actions.py
+++ sokoban/actions.py
@@ -97,6 +97,67 @@ def move_agent(board, direction):
             # we hit a wall, a box, or a goal that has a box on it, so we cannot move this specific box!
             return False
 
+def move_agent_mcts(board, direction):
+    result_square = ""
+    for row in range(0, len(board)):
+        for column in range(0, len(board[row])):
+            # our agent could be in an empty square (4) or on a goal (1)
+            if board[row][column] == "4" or board[row][column] == "1":
+                if board[row][column] == "4":
+                    is_goal = False
+                else:
+                    is_goal = True
+                agent_row = row
+                agent_column = column
+                if direction == "L":
+                    result_square = board[row][column-1]
+                    target_row = row
+                    target_column = column - 1
+                elif direction == "R":
+                    result_square = board[row][column+1]
+                    target_row = row
+                    target_column = column + 1
+                elif direction == "U":
+                    result_square = board[row-1][column]
+                    target_row = row - 1
+                    target_column = column
+                elif direction == "D":
+                    result_square = board[row+1][column]
+                    target_row = row + 1
+                    target_column = column
+                else:
+                    raise ValueError("Bad direction!")
+    # result_square has what is there now!
+    if result_square == "-1":
+        # there is a wall, we cannot move!
+        return board
+    # we are moving the agent to the goal
+    elif result_square == "0":
+        board[target_row][target_column] = "1"
+        # is the agent leaving a goal?
+        if is_goal:
+            board[agent_row][agent_column] = "0"
+        else:
+            board[agent_row][agent_column] = "5"
+        return board
+    elif result_square == "5":
+        board[target_row][target_column] = "4"
+        if is_goal:
+            board[agent_row][agent_column] = "0"
+        else:
+            board[agent_row][agent_column] = "5"
+        return board
+    elif result_square == "3" or result_square == "2":
+        if result_square == "3":
+            is_box_goal = False
+        else:
+            is_box_goal = True
+        # we also have to check if we can move the box or not ... ugh
+        if direction == "L":
+            box_result_square = board[agent_row][agent_column-2]
+            box_target_row = agent_row
+            box_target_column = agent_column - 2
+        elif direction == "R":
             box_result_square = board[agent_row][agent_column+2]
             box_target_row = agent_row
             box_target_column = agent_column + 2
